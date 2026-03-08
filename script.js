b7004d21fec67d04709b2f9ebe10f5f3e052df55
sumonsheikh0 <sumon214579@gmail.com>
2026-03-08 13:35:06 +0600

    log in logic add by js

---
+++ script.js
@@ -0,0 +1,20 @@
+const loginForm = document.getElementById("loginForm");
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+
+loginForm.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+
+
+    if(username === "admin" && password === "admin123"){
+        window.location.href = "home.html";
+
+    } else {   
+        alert("Wrong username or password");
+    }
+
+});
+
